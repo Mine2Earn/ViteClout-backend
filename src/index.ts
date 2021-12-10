704bd22353a70e8cc01b82db589978c81c2c06ac
Mathis Beauville <mathis.beauville@gmail.com>
2021-12-10 21:55:23 +0100

    Fixing passport no session found error

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(cookieParser());
-app.use(session({ secret: 'jsuispassecure dedi a eka', resave: true, saveUninitialized: true }));
+app.use(session({ secret: 'jsuispassecure dedi a eka', resave: true, saveUninitialized: true, cookie: { sameSite: false } }));
 app.use(passport.initialize());
 app.use(passport.session());
 
