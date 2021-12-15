0076f87058dde6aa0fa90c3d608451a38bfed97f
Mathis Beauville <mathis.beauville@gmail.com>
2021-12-15 12:44:43 +0100

    Add twitter query endpoint

--- src/routes/vuilders.ts
+++ src/routes/vuilders.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { addressFromTag, tagFromAddress, info } from '../controllers/vuilders';
+import { addressFromTag, tagFromAddress, info, twitterInfo } from '../controllers/vuilders';
 const router = Router();
 
 router.get('/addressfromtag', addressFromTag);
 router.get('/tagfromaddress', tagFromAddress);
 router.get('/infofromtwt', info);
+router.get('/twitterinfo', twitterInfo);
 
 export default router;
